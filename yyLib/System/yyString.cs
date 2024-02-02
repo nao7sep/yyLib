ceefc4b7e355edc1bd40e6d14de46011f1816882
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-02-02 11:24:48 +0900

    Added EmptyToNull

--- yyLib/System/yyString.cs
+++ yyLib/System/yyString.cs
@@ -5,9 +5,14 @@
         // c# - ?? Coalesce for empty string? - Stack Overflow
         // https://stackoverflow.com/questions/2420125/coalesce-for-empty-string
 
-        public static string? ConvertNullOrEmptyTo (this string? str, string? defaultString = null) => string.IsNullOrEmpty (str) == false ? str : defaultString;
+        // I, at first, implemented something like ConvertNullOrEmptyTo,
+        //     but if the method is designed to be called following a possibly null property (like Hoge?.Moge.Convert...),
+        //     the method may not be called at all and the whole line may return null.
+        // It's safer to convert each part to null if necessary and continue using the ?? operator.
 
-        public static string? ConvertNullOrWhiteSpaceTo (this string? str, string? defaultString = null) => string.IsNullOrWhiteSpace (str) == false ? str : defaultString;
+        public static string? EmptyToNull (this string? str) => string.IsNullOrEmpty (str) ? null : str;
+
+        public static string? WhiteSpaceToNull (this string? str) => string.IsNullOrWhiteSpace (str) ? null : str;
 
         public static string GetVisibleString (this string? str)
         {
