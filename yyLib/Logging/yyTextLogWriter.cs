d5f9f1da67a3abe3cc41c640cc48ebd0de511fb7
Yoshinao Inoguchi <apple@inoguchi.com>
2024-09-17 12:33:02 +0900

    Rename logging-related things

---
+++ yyLib/Logging/yyTextLogWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace yyLib
+{
+    public class yySimpleLogTextWriter (string filePath, Encoding? encoding = null)
+    {
+        public string FilePath { get; private set; } = filePath;
+
+        public Encoding Encoding { get; private set; } = encoding ?? Encoding.UTF8;
+
+        public void Write (DateTime creationUtc, string key, string value)
+        {
+            StringBuilder xBuilder = new ();
+
+            if (File.Exists (FilePath))
+                xBuilder.AppendLine ("----");
+
+            xBuilder.AppendLine ($"Time: {creationUtc.ToRoundtripString ()}");
+            xBuilder.AppendLine ($"{key}: {value.TrimRedundantLines ()}"); // Auto trimmed.
+
+            yyDirectory.CreateParent (FilePath);
+            File.AppendAllText (FilePath, xBuilder.ToString (), Encoding);
+        }
+    }
+}
