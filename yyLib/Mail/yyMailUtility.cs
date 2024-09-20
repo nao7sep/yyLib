0786fc16166b21851afbeeb51977a895e48e20a8
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-09-20 14:35:19 +0900

    Add yyMailUtility

---
+++ yyLib/Mail/yyMailUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace yyLib
+{
+    public static class yyMailUtility
+    {
+        public static async Task <(string JsonString, string MimeString, string SendingResult)> SendAsync (yyMailConnectionInfo connectionInfo, yyMailMessage message)
+        {
+            string xJsonString = JsonSerializer.Serialize (message, yyJson.DefaultSerializationOptions);
+
+            using MimeMessage xMimeMessage = new ();
+            xMimeMessage.Load (message);
+            string xMimeString = xMimeMessage.ToString ();
+
+            using SmtpClient xSmtpClient = new ();
+            await xSmtpClient.ConnectAsync (connectionInfo);
+            await xSmtpClient.AuthenticateAsync (connectionInfo);
+            string xSendingResult = await xSmtpClient.SendAsync (xMimeMessage);
+
+            return (JsonString: xJsonString, MimeString: xMimeString, SendingResult: xSendingResult);
+        }
+    }
+}
