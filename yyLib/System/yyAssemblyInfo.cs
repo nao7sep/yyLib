6752b944d18ad14d949c9f3ea368e2dc80018297
Yoshinao Inoguchi <apple@inoguchi.com>
2024-09-25 16:30:46 +0900

    Lazy-loading

--- yyLib/System/yyAssemblyInfo.cs
+++ yyLib/System/yyAssemblyInfo.cs
@@ -9,9 +9,9 @@ namespace yyLib
     // https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/Reflection/Assembly.cs
     // https://source.dot.net/#System.Private.CoreLib/src/System/Reflection/RuntimeAssembly.cs
 
-    public class yyAssemblyInfo (Assembly assembly)
+    public class yyAssemblyInfo
     {
-        public Assembly Assembly { get; init; } = assembly;
+        public Assembly Assembly { get; init; }
 
         // -----------------------------------------------------------------------------
         //     Used Frequently
@@ -28,42 +28,45 @@ namespace yyLib
         //     From Metadata
         // -----------------------------------------------------------------------------
 
-        private string? _assemblyName;
+        // The initialization of some of the following Lazy properties occur in the constructor
+        // because they depend on the Assembly property, that is unavailable upon the class's instantiation.
+
+        private readonly Lazy <string?> _assemblyName;
 
         /// <summary>
         /// Used as the output file name. Refer to AssemblyTitle for the assembly's display name.
         /// </summary>
-        public string? AssemblyName => _assemblyName ??= Assembly.GetName ().Name;
+        public string? AssemblyName => _assemblyName.Value;
 
-        private string? _assemblyTitle;
+        private readonly Lazy <string?> _assemblyTitle;
 
         /// <summary>
         /// The assembly's display name. Refer to this rather than AssemblyName, that is the output file name.
         /// </summary>
-        public string? AssemblyTitle => _assemblyTitle ??= Assembly.GetCustomAttribute <AssemblyTitleAttribute> ()?.Title;
+        public string? AssemblyTitle => _assemblyTitle.Value;
 
-        private string? _company;
+        private readonly Lazy <string?> _company;
 
-        public string? Company => _company ??= Assembly.GetCustomAttribute <AssemblyCompanyAttribute> ()?.Company;
+        public string? Company => _company.Value;
 
-        private string? _product;
+        private readonly Lazy <string?> _product;
 
-        public string? Product => _product ??= Assembly.GetCustomAttribute <AssemblyProductAttribute> ()?.Product;
+        public string? Product => _product.Value;
 
-        private string? _description;
+        private readonly Lazy <string?> _description;
 
-        public string? Description => _description ??= Assembly.GetCustomAttribute <AssemblyDescriptionAttribute> ()?.Description;
+        public string? Description => _description.Value;
 
-        private string? _copyright;
+        private readonly Lazy <string?> _copyright;
 
-        public string? Copyright => _copyright ??= Assembly.GetCustomAttribute <AssemblyCopyrightAttribute> ()?.Copyright;
+        public string? Copyright => _copyright.Value;
 
-        private string? _configuration;
+        private readonly Lazy <string?> _configuration;
 
         /// <summary>
         /// You may not get what you expect from the .csproj file because the compiler may override it.
         /// </summary>
-        public string? Configuration => _configuration ??= Assembly.GetCustomAttribute <AssemblyConfigurationAttribute> ()?.Configuration;
+        public string? Configuration => _configuration.Value;
 
         // Versions are confusing.
 
@@ -81,30 +84,47 @@ namespace yyLib
         // It may also be useful when the assembly is released as a NuGet package because we wont be needing to update 2 version strings.
         // The best practice should be: Specifying "Version" and programmatically referring to the AssemblyVersion property.
 
-        private string? _version;
+        private readonly Lazy <Version?> _version;
 
         /// <summary>
         /// The NuGet package version. Refer to AssemblyVersion for the assembly's version.
         /// </summary>
-        public string? Version => _version ??= Assembly.GetName ().Version?.ToString ();
+        public Version? Version => _version.Value;
 
-        private string? _assemblyVersion;
+        private readonly Lazy <Version?> _assemblyVersion;
 
         /// <summary>
         /// The assembly's version. Refer to this rather than Version, that is the NuGet package version.
         /// </summary>
-        public string? AssemblyVersion => _assemblyVersion ??= Assembly.GetName ().Version?.ToString ();
+        public Version? AssemblyVersion => _assemblyVersion.Value;
 
-        private string? _fileVersion;
+        private readonly Lazy <string?> _fileVersion;
 
-        public string? FileVersion => _fileVersion ??= Assembly.GetCustomAttribute <AssemblyFileVersionAttribute> ()?.Version;
+        public string? FileVersion => _fileVersion.Value;
 
-        private string? _informationalVersion;
+        private readonly Lazy <string?> _informationalVersion;
 
         /// <summary>
         /// Returns something like: 0.1+03073b01cc6d77db9dfaab1b7411d7a940e54eb2
         /// </summary>
-        public string? InformationalVersion => _informationalVersion ??= Assembly.GetCustomAttribute <AssemblyInformationalVersionAttribute> ()?.InformationalVersion;
+        public string? InformationalVersion => _informationalVersion.Value;
+
+        public yyAssemblyInfo (Assembly assembly)
+        {
+            Assembly = assembly;
+
+            _assemblyName = new (() => Assembly.GetName ().Name);
+            _assemblyTitle = new (() => Assembly.GetCustomAttribute <AssemblyTitleAttribute> ()?.Title);
+            _company = new (() => Assembly.GetCustomAttribute <AssemblyCompanyAttribute> ()?.Company);
+            _product = new (() => Assembly.GetCustomAttribute <AssemblyProductAttribute> ()?.Product);
+            _description = new (() => Assembly.GetCustomAttribute <AssemblyDescriptionAttribute> ()?.Description);
+            _copyright = new (() => Assembly.GetCustomAttribute <AssemblyCopyrightAttribute> ()?.Copyright);
+            _configuration = new (() => Assembly.GetCustomAttribute <AssemblyConfigurationAttribute> ()?.Configuration);
+            _version = new (() => Assembly.GetName ().Version);
+            _assemblyVersion = new (() => Assembly.GetName ().Version);
+            _fileVersion = new (() => Assembly.GetCustomAttribute <AssemblyFileVersionAttribute> ()?.Version);
+            _informationalVersion = new (() => Assembly.GetCustomAttribute <AssemblyInformationalVersionAttribute> ()?.InformationalVersion);
+        }
 
         /// <summary>
         /// For debugging purposes.
@@ -112,6 +132,9 @@ namespace yyLib
         public string? GetVisibleStrings (string? newLine = null) // Plural.
         {
             List <string> xLines = new ();
+
+            // Version.ToString refers to DefaultFormatFieldCount and generates a 2-3 field string if the trailing fields are not specified.
+            // https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/Version.cs
 
             xLines.Add ($"Location: {Location.GetVisibleString ()}");
             xLines.Add ($"FullName: {FullName.GetVisibleString ()}");
@@ -122,8 +145,8 @@ namespace yyLib
             xLines.Add ($"Description: {Description.GetVisibleString ()}");
             xLines.Add ($"Copyright: {Copyright.GetVisibleString ()}");
             xLines.Add ($"Configuration: {Configuration.GetVisibleString ()}");
-            xLines.Add ($"Version: {Version.GetVisibleString ()}");
-            xLines.Add ($"AssemblyVersion: {AssemblyVersion.GetVisibleString ()}");
+            xLines.Add ($"Version: {(Version?.ToString ()).GetVisibleString ()}");
+            xLines.Add ($"AssemblyVersion: {(AssemblyVersion?.ToString ()).GetVisibleString ()}");
             xLines.Add ($"FileVersion: {FileVersion.GetVisibleString ()}");
             xLines.Add ($"InformationalVersion: {InformationalVersion.GetVisibleString ()}");
 
