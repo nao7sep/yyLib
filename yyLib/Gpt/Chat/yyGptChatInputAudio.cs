dd001ccea5916056e1094950890c5345e113d7e3
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-01-22 19:14:43 +0900

    yyGptChatMessageContent added

---
+++ yyLib/Gpt/Chat/yyGptChatInputAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace yyLib
+{
+    public class yyGptChatInputAudio
+    {
+        [JsonPropertyName ("data")]
+        public string? Data { get; set; }
+
+        [JsonPropertyName ("format")]
+        public string? Format { get; set; }
+    }
+}
---
+++ yyLib/Gpt/Chat/yyGptChatMessageContent.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace yyLib
+{
+    public class yyGptChatMessageContent
+    {
+        [JsonPropertyName ("type")]
+        public string? Type { get; set; }
+
+        [JsonPropertyName ("text")]
+        public string? Text { get; set; }
+
+        [JsonPropertyName ("image_url")]
+        public yyGptChatImageUrl? ImageUrl { get; set; }
+
+        [JsonPropertyName ("input_audio")]
+        public yyGptChatInputAudio? InputAudio { get; set; }
+    }
+}
