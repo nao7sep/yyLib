60181019f8f8c8e14d9455b5fa241db7b3bffd10
Yoshinao Inoguchi <apple@inoguchi.com>
2025-03-28 16:08:50 +0900

    cleaning #4

---
+++ yyLib/Exceptions/yyNullReferenceException.cs
@@ -0,0 +1,13 @@
+﻿namespace yyLib
+{
+    public class yyNullReferenceException: yyException
+    {
+        public yyNullReferenceException (string message): base (message)
+        {
+        }
+
+        public yyNullReferenceException (string message, Exception innerException): base (message, innerException)
+        {
+        }
+    }
+}
